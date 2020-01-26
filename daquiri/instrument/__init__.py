566b3d8db924baf476c69284a8d6178e0f69a1bf
Conrad Stansbury <chstansbury@gmail.com>
2020-01-25 17:09:20 -0800

    Remove bad import

--- daquiri/instrument/__init__.py
+++ daquiri/instrument/__init__.py
@@ -1,4 +1,4 @@
-from .axis import Axis, Detector
+from .axis import Axis
 from .managed_instrument import ManagedInstrument
 from .spec import AxisSpecification, AxisListSpecification, LogicalAxisSpecification
 from .property import Property
