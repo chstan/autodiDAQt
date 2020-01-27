7f8bcf73d4d55cf43c08cae12a1319213a551269
Conrad Stansbury <chstansbury@gmail.com>
2020-01-27 10:17:52 -0800

    Retain profiling logs

--- profiling/profile_all.py
+++ profiling/profile_all.py
@@ -1,9 +1,14 @@
+import datetime
 import subprocess, sys
 from pathlib import Path
 
+profiling_path = Path(__file__).parent
+results_path = profiling_path / 'results'
+ps_file = profiling_path / 'generate_profiling_data.ps1'
+
 
 def generate_profiling_results():
-    ps_file = Path(__file__).parent / 'generate_profiling_data.ps1'
+
     print(str(ps_file.absolute()))
     p = subprocess.Popen(["powershell.exe", str(ps_file.absolute()),
                           '-rootDir', str(ps_file.parent.absolute())],
@@ -11,5 +16,16 @@ def generate_profiling_results():
 
     p.communicate()
 
+
+def move_profiling_results_to(dest_folder: Path):
+    dest_folder.mkdir()
+    for profile in results_path.glob('*.html'):
+        profile.rename(dest_folder / profile.name)
+
+
 if __name__ == '__main__':
+    today = datetime.datetime.today()
+    dest_folder = today.isoformat().split('.')[0].replace(':', '-')
+
     generate_profiling_results()
+    move_profiling_results_to(results_path / dest_folder)
