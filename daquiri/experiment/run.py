178e2d8b47a8b5bd6375d393ca69d6ab6663cd69
Conrad Stansbury <chstansbury@gmail.com>
2021-02-14 15:46:42 -0800

    Refactor saving into classes
    1. Cleaner
    2. You can provide several save formats

--- daquiri/experiment/run.py
+++ daquiri/experiment/run.py
@@ -1,19 +1,77 @@
 import datetime
 import functools
-import json
 import operator
 import pickle
 import warnings
 from collections import defaultdict
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union, Type
 
 import numpy as np
 import xarray as xr
-from daquiri.utils import RichEncoder
+
+from .save import RunSaver, SaveContext, save_cls_from_short_name
 
 __all__ = ["Run"]
+
+SaveFormat = Union[str, Type[RunSaver]]
+
+def daq_to_timesequence_xarray(stream_name: str, data_stream) -> xr.Dataset:
+    """
+    Data streams are always lists of dictionaries with a
+    point, a step number, and the acquisition time. Here we
+
+    Args:
+        stream_name:
+        data_stream:
+
+    Returns:
+        xr.Dataset: All accumulated data as an xr.Dataset
+        with dims and appropriate coords for the DAQ session.
+    """
+    step, points, data, time = [
+        [p[name] for p in data_stream]
+        for name in ["step", "point", "data", "time"]
+    ]
+    time = np.vectorize(np.datetime64)(np.asarray(time))
+    time_dim = f"{stream_name}-time"
+
+    peeked = data[0]
+    
+    # if the data consists of numpy arrays and they are the same shape, then we can
+    # create dimensions and axes for them. It would probably be better to specify this
+    # more directly. A few possible mechanisms exist:
+    #   - Allow data schemas to specify how they collate data/multiple observations
+    #   - Look at the schema value and special case ArrayType from ObjectType
+    if isinstance(peeked, np.ndarray) and functools.reduce(operator.eq, [arr.shape for arr in data]):
+        data = np.stack(data, axis=-1)
+        data_coords = {
+            f"dim_{i}": np.arange(s) for i, s in enumerate(peeked.shape)
+        }
+        data_coords[time_dim] = time
+        data_dims = [f"dim_{i}" for i in range(len(peeked.shape))] + [time_dim]
+    else:
+        data = np.asarray(data)
+        data_coords = {f"{stream_name}-time": time}
+        data_dims = [time_dim]
+
+    time_coords = {f"{stream_name}-time": time}
+    ds = xr.Dataset(
+        {
+            f"{stream_name}-step": xr.DataArray(
+                np.asarray(step), coords=time_coords, dims=[time_dim],
+            ),
+            f"{stream_name}-point": xr.DataArray(
+                np.asarray(points), coords=time_coords, dims=[time_dim],
+            ),
+            f"{stream_name}-data": xr.DataArray(
+                data, coords=data_coords, dims=data_dims,
+            ),
+        }
+    )
+    return ds
+
 
 @dataclass
 class Run:
@@ -83,110 +141,35 @@ class Run:
         directory.mkdir(parents=True, exist_ok=True)
         return directory
 
-    def save(self, save_directory: Path, extra=None, extra_attrs=None, save_format="zarr"):
-        if extra is None:
-            extra = {}
+    def save(self, save_directory: Path, extra=None, extra_attrs=None, save_format=Union[SaveFormat, List[SaveFormat]]):
+        # first, normalize all the formats to the respective classes
+        if not isinstance(save_format, (list, tuple)):
+            save_format = [save_format]
 
-        if extra_attrs is None:
-            extra_attrs = {}
+        save_format = [
+            save_cls_from_short_name(format) if isinstance(format, str) else format 
+            for format in save_format
+        ]
 
-        with open(str(save_directory / "metadata-small.json"), "w+") as f:
-            json.dump({"metadata": self.metadata,}, f, cls=RichEncoder, indent=2)
-
-        with open(str(save_directory / "metadata.json"), "w+") as f:
-            json.dump(
-                {
-                    "metadata": self.metadata,
-                    "point_started": self.point_started,
-                    "point_ended": self.point_ended,
-                    "steps_taken": self.steps_taken,
-                },
-                f,
-                cls=RichEncoder,
-                indent=2,
-            )
-
-        def daq_to_xarray(stream_name, data_stream) -> xr.Dataset:
-            """
-            Data streams are always lists of dictionaries with a
-            point, a step number, and the acquisition time. Here we
-
-            Args:
-                data_stream:
-
-            Returns:
-                xr.Dataset: All accumulated data as an xr.Dataset
-                with dims and appropriate coords for the DAQ session.
-            """
-            step, points, data, time = [
-                [p[name] for p in data_stream]
-                for name in ["step", "point", "data", "time"]
-            ]
-            time = np.vectorize(np.datetime64)(np.asarray(time))
-            time_dim = f"{stream_name}-time"
-
-            peeked = data[0]
-            
-            # if the data consists of numpy arrays and they are the same shape, then we can
-            # create dimensions and axes for them. It would probably be better to specify this
-            # more directly. A few possible mechanisms exist:
-            #   - Allow data schemas to specify how they collate data/multiple observations
-            #   - Look at the schema value and special case ArrayType from ObjectType
-            if isinstance(peeked, np.ndarray) and functools.reduce(operator.eq, [arr.shape for arr in data]):
-                data = np.stack(data, axis=-1)
-                data_coords = {
-                    f"dim_{i}": np.arange(s) for i, s in enumerate(peeked.shape)
-                }
-                data_coords[time_dim] = time
-                data_dims = [f"dim_{i}" for i in range(len(peeked.shape))] + [time_dim]
-            else:
-                data = np.asarray(data)
-                data_coords = {f"{stream_name}-time": time}
-                data_dims = [time_dim]
-
-            ds = xr.Dataset(
-                {
-                    f"{stream_name}-step": xr.DataArray(
-                        np.asarray(step),
-                        coords={f"{stream_name}-time": time},
-                        dims=[time_dim],
-                    ),
-                    f"{stream_name}-point": xr.DataArray(
-                        np.asarray(points),
-                        coords={f"{stream_name}-time": time},
-                        dims=[time_dim],
-                    ),
-                    f"{stream_name}-data": xr.DataArray(
-                        data, coords=data_coords, dims=data_dims,
-                    ),
-                }
-            )
-            return ds
-
+        # prep metadata and data for save
+        all_metadata = {
+            "metadata": self.metadata,
+            "point_started": self.point_started,
+            "point_ended": self.point_ended,
+            "steps_taken": self.steps_taken,
+        }
         daq = xr.merge(
             [
-                daq_to_xarray("-".join(str(k) for k in ks), v)
+                daq_to_timesequence_xarray("-".join(map(str, ks)), v)
                 for ks, v in self.daq_values.items()
             ]
         )
-        daq = daq.assign_attrs(extra_attrs)
+        daq = daq.assign_attrs({} if extra_attrs is None else extra_attrs)
 
-        if save_format == "zarr":
-            daq.to_zarr(save_directory / "raw_daq.zarr")
-
-            for k, v in extra.items():
-                if v is None:
-                    continue
-
-                v.to_zarr(save_directory / f"{k}.zarr")
-        elif save_format == "pickle":
-            with open(str(save_directory / "raw_daq.pickle"), "wb+") as fpickle:
-                pickle.dump(daq, fpickle, protocol=-1)
-            
-            for k, v in extra.items():
-                if v is None:
-                    continue
-
-                with open(str(save_directory / f"{k}.pickle"), "wb+") as fpickle:
-                    pickle.dump(v, fpickle, protocol=-1)
+        # for each specified format, save the data
+        format: Type[RunSaver]
+        for format in save_format:
+            save_context = SaveContext(save_directory / format.short_name)
+            format.save_run(all_metadata, daq, save_context)
+            format.save_user_extras(extra or {}, save_context)
 
