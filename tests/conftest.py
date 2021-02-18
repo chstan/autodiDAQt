ce2cff57fd0e5973c3586ad6a68abf2cc5668313
Conrad Stansbury <chstansbury@gmail.com>
2021-02-17 19:50:31 -0800

    Fixtures for experiments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-# need to mock pyqt_led because it does not work on the CI server
+# need to monkeypatch pyqt_led because it does not work on the CI server
+from daquiri.core import make_user_data_dataclass
 import sys
 
 
@@ -11,10 +12,12 @@ module.Led = Led
 sys.modules['pyqt_led'] = module
 
 from typing import Dict
+from pathlib import Path
 
-import pytest
+from daquiri.experiment.save import ZarrSaver
 from daquiri import Daquiri, Actor
 from daquiri.collections import AttrDict
+from daquiri.mock import MockMotionController, MockScalarDetector
 from daquiri.config import Config, default_config_for_platform, MetaData
 from daquiri.instrument import ManagedInstrument
 from daquiri.state import AppState
@@ -26,7 +29,10 @@ class MockDaquiri(Daquiri):
 
     def __init__(self, *args, **kwargs):
         self._instruments = {}
+        self.managed_instrument_classes = {}
         self._actors = {}
+        self.panel_definitions = {}
+
         self.config = Config(default_config_for_platform())
         self.meta = MetaData()
         self.app_state = AppState()
@@ -45,12 +51,16 @@ class MockDaquiri(Daquiri):
     def init_with(self, managed_instruments=None, panels=None):
         if managed_instruments is None:
             managed_instruments = {}
+
         if panels is None:
             panels = {}
 
         for k, ins_cls in managed_instruments.items():
             ins = ins_cls(app=self)
             self._instruments[k] = ins
+            self.managed_instrument_classes[k] = ins_cls
+
+        self.panel_definitions.update(panels)
 
     def cleanup(self):
         print('Cleanup')
@@ -79,3 +89,36 @@ def app():
     app = MockDaquiri()
     yield app
     app.cleanup()
+
+@pytest.fixture(scope="function")
+def experiment_cls():
+    return None
+
+@pytest.fixture(scope="function")
+def instrument_classes():
+    return None
+
+@pytest.fixture(scope="function")
+async def experiment(app, mocker, caplog, experiment_cls, instrument_classes):
+    if experiment_cls is None:
+        experiment_cls = BasicExperiment
+
+    if instrument_classes is None:
+        instrument_classes = {
+            'mc': MockMotionController,
+            'power_meter': MockScalarDetector,
+        }
+
+    app.init_with(instrument_classes)
+
+    mocker.patch.object(ZarrSaver, "save_run")
+    mocker.patch.object(ZarrSaver, "save_metadata")
+    mocker.patch.object(ZarrSaver, "save_user_extras")
+    mocker.patch.object(Path, "mkdir")
+
+    exp = experiment_cls(app)
+    app.actors["experiment"] = exp
+    await exp.prepare()
+
+    yield exp
+
